d5b4ee8947370f3c7c61db65d29ee60ed9a62d03
singjun <shinsj4653@gmail.com>
2023-02-13 18:13:47 +0900

    feat: baekjoon 11655
    미해결 - 풀이 참고하여 issue에 정리

---
+++ 6장_문자열 조작/11655.py
@@ -0,0 +1,19 @@
+def sol() :
+    # 문자열 : 조작 불가능
+    # 새로운 변수 ans
+    s = input()
+    ans = []
+    for idx, letter in enumerate(s) :
+        if letter == " " :
+            ans.append(" ")
+        elif letter.isdigit() :
+            ans.append(str(letter))
+        else :
+            if chr(ord(letter) + 13).isalnum() :
+                ans.append(chr(ord(letter) + 13))
+            else :
+                ans.append(chr(ord(letter) + 13 - 26))
+    return "".join(ans)
+
+if __name__ == '__main__':
+    print(sol())
