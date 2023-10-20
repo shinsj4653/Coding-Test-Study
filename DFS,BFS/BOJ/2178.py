066702c8a91bac72d684b4d57d3920a9663f4bd6
SEONG JUN SHIN <shinsj4653@konkuk.ac.kr>
2023-10-20 18:38:40 +0900

    feat: BOJ_2178_진행주웅

--- DFS,BFS/BOJ/2178.py
+++ DFS,BFS/BOJ/2178.py
@@ -1,10 +1,13 @@
 import sys
+from collections import deque
+
 input = sys.stdin.readline
 
 # 4, 6
 n, m = map(int, input().split())
 
 maze = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+chk = [[False for _ in range(m + 1)] for _ in range(n + 1)]
 
 for i in range(n) : # 0 ~ 3
     row = input()
@@ -13,7 +16,28 @@ for i in range(n) : # 0 ~ 3
         maze[i + 1][j + 1] = int(row[j])
 
 print(maze)
-
 answer = 0
+
+
+dx = [-1, 1, 0, 0] # 북, 남, 동, 서
+dy = [0, 0, 1, -1]
+
+stack = []
+
 def bfs(x, y) :
-    
+    global answer
+    stack.append((x, y))
+
+    #x, y = queue.popleft()
+
+    for i in range(4) :
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        bfs()
+
+
+
+
+
+
